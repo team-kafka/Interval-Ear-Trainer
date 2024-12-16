7a327320b71b496beb885c96c76f082be7c33f44
Nicolas Carre <nicolas.carre@gmail.com>
2024-12-16 17:09:06 +0900

    Set background mode for audio and first stab at a function for playing multiple notes

--- Interval Ear Trainer/MidiPlayer.swift
+++ Interval Ear Trainer/MidiPlayer.swift
@@ -8,6 +8,7 @@
 import Foundation
 import AVFoundation
 
+
 class MidiPlayer {
     var midiPlayer: AVMIDIPlayer?
     var bankURL: URL
@@ -17,6 +18,16 @@ class MidiPlayer {
             fatalError("\"Arnold___David_Classical_Piano.sf2\" file not found.")
         }
         self.bankURL = bankURL
+        
+        // Avoid stopping sounds upon screen lock
+        let session = AVAudioSession.sharedInstance()
+        do {
+            try session.setCategory(AVAudioSession.Category.playback,
+                                    options: [])
+        } catch let error as NSError {
+            print("Failed to set the audio session category and mode: \(error.localizedDescription)")
+        }
+
     }
     
     func prepareSong(note:Int, duration:Double){
@@ -36,6 +47,7 @@ class MidiPlayer {
         guard MusicTrackNewMIDINoteEvent(track!, MusicTimeStamp(0), &musicNote) == OSStatus(noErr) else {
             fatalError("Cannot add Note")
         }
+        
         guard MusicSequenceFileCreateData(musicSequence!,
                                           MusicSequenceFileTypeID.midiType,
                                           MusicSequenceFileFlags.eraseFile,
@@ -62,6 +74,60 @@ class MidiPlayer {
     }
     func playNote(note:Int, duration: Double){
         self.prepareSong(note: note, duration:duration)
+        self.playSong()
+    }
+    
+    //test playing whole stream of notes in one go
+    func prepare_sequence(notes:[Int], duration:Double) -> MusicSequence {
+        
+        var musicSequence: MusicSequence?
+        NewMusicSequence(&musicSequence)
+        var track: MusicTrack?
+        
+        guard MusicSequenceNewTrack(musicSequence!, &track) == OSStatus(noErr) else {
+            fatalError("Cannot add track")
+        }
+        
+        var position = 0.0
+        for note in notes{
+            var musicNote = MIDINoteMessage(channel: 0,
+                                            note: UInt8(note),
+                                            velocity: 64,
+                                            releaseVelocity: 0,
+                                            duration: Float(duration))
+            
+            guard MusicTrackNewMIDINoteEvent(track!, MusicTimeStamp(position), &musicNote) == OSStatus(noErr) else {
+                fatalError("Cannot add Note")
+            }
+            position += duration
+        }
+        return musicSequence!
+    }
+    
+    func prepare_song2(musicSequence: MusicSequence){
+        var data: Unmanaged<CFData>?
+        guard MusicSequenceFileCreateData(musicSequence,
+                                          MusicSequenceFileTypeID.midiType,
+                                          MusicSequenceFileFlags.eraseFile,
+                                          480, &data) == OSStatus(noErr) else {
+            fatalError("Cannot create music midi data")
+        }
+        
+        if let md = data {
+            let midiData = md.takeUnretainedValue() as Data
+            do {
+                try self.midiPlayer = AVMIDIPlayer(data: midiData, soundBankURL: self.bankURL)
+            } catch let error {
+                fatalError(error.localizedDescription)
+            }
+        }
+        self.midiPlayer!.prepareToPlay()
+    }
+    
+    
+    func playNotes(notes:[Int], duration: Double){
+        var musicSequence = self.prepare_sequence(notes: notes, duration:duration)
+        self.prepare_song2(musicSequence: musicSequence)
         self.playSong()
     }
     
